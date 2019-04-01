59366a4dfa21a68dea87599d0cc42d816128ff7e
LukBukkit <luk.bukkit@gmail.com>
2019-04-01 20:42:44 +0200

    Using UTF-8 for creating database tables

--- src/main/java/de/lukweb/timetablebot/sql/DB.java
+++ src/main/java/de/lukweb/timetablebot/sql/DB.java
@@ -31,7 +31,7 @@ public class DB {
                     "  `id` int(11) NOT NULL AUTO_INCREMENT, " +
                     "  `name` varchar(100) DEFAULT NULL UNIQUE, " +
                     "  PRIMARY KEY (`id`) " +
-                    ") ENGINE=InnoDB DEFAULT CHARSET=latin1");
+                    ") ENGINE=InnoDB DEFAULT CHARSET=utf8");
             handle.execute("CREATE TABLE IF NOT EXISTS `users` ( " +
                     "  `chatid` INT(11) NOT NULL, " +
                     "  `blocked` INT(1) NOT NULL, " +
@@ -44,7 +44,7 @@ public class DB {
                     "  `created` int(16) DEFAULT NULL, " +
                     "  `updated` int(16) DEFAULT NULL, " +
                     "  UNIQUE KEY `users_chatid_uindex` (`chatid`) " +
-                    ") ENGINE=InnoDB DEFAULT CHARSET=latin1");
+                    ") ENGINE=InnoDB DEFAULT CHARSET=utf8");
             handle.execute("CREATE TABLE IF NOT EXISTS `amendments` ( " +
                     " `id` INT(11) NOT NULL AUTO_INCREMENT, " +
                     " `type` VARCHAR(99) NOT NULL, " +
@@ -58,7 +58,7 @@ public class DB {
                     " `addtional_information` MEDIUMTEXT NOT NULL," +
                     " `deleted` int(1) DEFAULT 0 NULL, " +
                     " PRIMARY KEY (`id`) " +
-                    ") ENGINE=InnoDB DEFAULT CHARSET=latin1");
+                    ") ENGINE=InnoDB DEFAULT CHARSET=utf8");
             handle.execute("CREATE TABLE IF NOT EXISTS `feedback` ( " +
                     "  `id` INT(11) NOT NULL AUTO_INCREMENT, " +
                     "  `from` INT(11) DEFAULT NULL, " +
@@ -66,7 +66,7 @@ public class DB {
                     "  `timestamp` INT(11) DEFAULT NULL," +
                     "  CONSTRAINT `feedback_users_chatid_fk` FOREIGN KEY (`from`) REFERENCES `users` (`chatid`) ON DELETE CASCADE ON UPDATE CASCADE, " +
                     "  PRIMARY KEY (`id`) " +
-                    ") ENGINE=InnoDB DEFAULT CHARSET=latin1");
+                    ") ENGINE=InnoDB DEFAULT CHARSET=utf8");
             handle.execute("CREATE TABLE IF NOT EXISTS `users_teachers` ( " +
                     "  `user` int(11) NOT NULL, " +
                     "  `teacher` int(11) NOT NULL, " +
@@ -74,14 +74,14 @@ public class DB {
                     "  KEY `userTeachers_teachers_id_fk` (`teacher`), " +
                     "  CONSTRAINT `userTeachers_teachers_id_fk` FOREIGN KEY (`teacher`) REFERENCES `teachers` (`id`) ON DELETE CASCADE ON UPDATE CASCADE, " +
                     "  CONSTRAINT `userTeachers_users_chatid_fk` FOREIGN KEY (`user`) REFERENCES `users` (`chatid`) ON DELETE CASCADE ON UPDATE CASCADE " +
-                    ") ENGINE=InnoDB DEFAULT CHARSET=latin1");
+                    ") ENGINE=InnoDB DEFAULT CHARSET=utf8");
             handle.execute("CREATE TABLE IF NOT EXISTS cafeteria ( " +
                     "    day int, " +
                     "    vegetarian tinyint, " +
                     "    meal VARCHAR(300), " +
                     "    price double, " +
                     "  PRIMARY KEY (`day`,`vegetarian`)" +
-                    ") ENGINE=InnoDB DEFAULT CHARSET=latin1");
+                    ") ENGINE=InnoDB DEFAULT CHARSET=utf8");
         });
     }
 
