4d506d2cffc8ada8b7f4472ef64ff3b688a39999
LukBukkit <luk.bukkit@gmail.com>
2019-04-05 19:38:39 +0200

    Forcing UTF-8 encoding on Linux

--- src/main/java/de/lukweb/timetablebot/timetable/SvplanParser.java
+++ src/main/java/de/lukweb/timetablebot/timetable/SvplanParser.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
 
 public abstract class SvplanParser {
 
@@ -37,7 +38,7 @@ public abstract class SvplanParser {
             // Using the http basic authorization the string 'authorization' should be Base64 Encoded
             // Generator: https://www.blitter.se/utils/basic-authentication-header-generator/
             connection.setRequestProperty("Authorization", "Basic " + authorization);
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
 
             StringBuilder string = new StringBuilder();
             String inputLine;
--- src/main/java/de/lukweb/timetablebot/timetable/repres/NonBreakingCleaner.java
+++ src/main/java/de/lukweb/timetablebot/timetable/repres/NonBreakingCleaner.java
@@ -1,20 +1,30 @@
 package de.lukweb.timetablebot.timetable.repres;
 
 import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NonBreakingCleaner {
 
     private Elements elements;
+    private Logger logger;
 
     public NonBreakingCleaner(Elements elements) {
         this.elements = elements;
+        this.logger = LoggerFactory.getLogger(getClass());
     }
 
     public String text(int index) {
         return clean(elements.get(index).text());
     }
 
-    public static String clean(String string) {
+    private String clean(String string) {
+        // This char would cause a MySQL error
+        char invalidChar = '\uFFFD';
+        if (string.indexOf(invalidChar) >= 0) {
+            logger.warn("Invalid char in web string '{}' found", string);
+        }
+
         // Replacing invalid characters
         return string.replace('\u00A0', ' ')
                 // This would cause a MySQL error
